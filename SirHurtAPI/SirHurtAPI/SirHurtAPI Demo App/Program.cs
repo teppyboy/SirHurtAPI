1501ce9c80824f5d46c0788fd0cf4b41b9a233ce
teppyboy <tretrauvodung123@gmail.com>
2019-10-05 23:39:09 +0700

    Upload files :)
    Not axon thanks

--- SirHurtAPI/SirHurtAPI/SirHurtAPI Demo App/Program.cs
+++ SirHurtAPI/SirHurtAPI/SirHurtAPI Demo App/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -11,9 +14,38 @@ namespace SirHurtAPI_Demo_App
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
+        [DllImport("kernel32.dll")]
+        public static extern IntPtr GetConsoleWindow();
+
+        [DllImport("user32.dll")]
+        public static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+        public const int SW_HIDE = 0;
+        public const int SW_SHOW = 5;
         [STAThread]
         static void Main()
         {
+            FileInfo fileInfo = new FileInfo(Assembly.GetExecutingAssembly().Location);
+            DateTime lastModified = fileInfo.LastWriteTime;
+            Console.Title = "SirHurtAPI Demo App Launcher";
+            Console.WriteLine("SirHurtAPI Demo App Launcher");
+            Console.WriteLine("Build: " + lastModified.ToShortDateString().Replace("/", ""));
+            Console.WriteLine("Build date: " + lastModified.ToString());
+            Console.WriteLine("======================================");
+            Console.WriteLine("Checking for required DLL...");
+            if (File.Exists("SirHurtAPI.dll"))
+            {
+                Console.WriteLine("SirHurtAPI.dll - FOUND");
+                Console.WriteLine("======================================");
+            }
+            else
+            {
+                Console.WriteLine("SirHurtAPI.dll - NOT FOUND");
+                Console.WriteLine("Downloading SirHurtAPI.dll");
+                //...
+                Console.WriteLine("Downloaded.");
+                Console.WriteLine("======================================");
+            }
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainForm());
